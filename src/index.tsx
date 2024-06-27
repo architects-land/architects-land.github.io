2e72392f3f58acb5e64e7f7a03df3ee48c0fad0e
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-06-27 23:24:32 +0000

    fix(root): animation stopped before end of season info

--- src/index.tsx
+++ src/index.tsx
@@ -10,11 +10,42 @@ render(() => <App />, root!)
 
 const seasons = document.getElementsByClassName('presentation__season')
 
+let blocked: Element[] = []
+
 for (const season of seasons) {
     season.addEventListener('mouseenter', _ => {
-        season.classList.add("presentation__season--active")
+        if (!blocked.includes(season)) {
+            season.classList.add("presentation__season--active")
+            addToBlocked(season)
+            return;
+        }
+        updateStatus(season, "presentation__season")
     })
     season.addEventListener('mouseleave', _ => {
-        season.classList.remove("presentation__season--active")
+        if (!blocked.includes(season)) {
+            season.classList.remove("presentation__season--active")
+            addToBlocked(season)
+            return;
+        }
+        updateStatus(season, "presentation__season", false)
     })
+}
+
+function updateStatus(el: Element, baseClass: string, add = true) {
+    setTimeout((_: any) => {
+        if (!blocked.includes(el)) {
+            if (add) el.classList.add(`${baseClass}--active`)
+            else el.classList.remove(`${baseClass}--active`)
+            addToBlocked(el)
+            return;
+        }
+        updateStatus(el, baseClass, add)
+    }, 10)
+}
+
+function addToBlocked(el: Element) {
+    blocked.push(el)
+    setTimeout((_: any) => {
+        blocked.splice(blocked.indexOf(el), 1)
+    }, 1000);
 }
